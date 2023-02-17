8ad55dadbbb6b6933ba2b05eadacd51f7c23c606
vino <vino0908@outlook.com>
2023-02-17 12:15:35 +0800

    use unocss in project

---
+++ unocss.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig, presetAttributify, presetIcons, presetUno, presetWebFonts } from 'unocss'
+
+export default defineConfig({
+  presets: [
+    presetUno(),
+    presetAttributify(),
+    presetIcons(),
+    presetWebFonts(),
+  ],
+})
