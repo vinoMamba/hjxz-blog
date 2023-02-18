3891811885379d542c66447fa973698e653a38fd
vino <vino0908@outlook.com>
2023-02-18 23:41:20 +0800

    feat: 使用 swr 获取 钉钉用户信息

---
+++ pages/api/login.ts
@@ -0,0 +1,31 @@
+import { Result, UserInfo } from '@/next-env'
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Result<UserInfo|null>>
+) {
+  req.headers['Content-Type'] = 'application/json'
+  if(req.method ==='POST'){
+    const token = await getAccessToken()
+    const result = await  fetch(`https://oapi.dingtalk.com/topapi/v2/user/getuserinfo?access_token=${token}`,{
+      method:'POST',
+      body:req.body,
+     })
+    const userData = await result.json()
+    if(userData.errcode === 0){
+      res.status(200).json({errcode:0,message:'获取用户信息成功',data:{userId:userData.result.userid,name:userData.result.name}})
+    }else{
+      res.status(500).json({errcode:1,message:'获取用户信息失败',data:null})
+    }
+  }
+}
+
+
+async function getAccessToken(){
+  const key= process.env.APP_KEY
+  const secret = process.env.APP_SECRET
+  const res = await fetch(`https://oapi.dingtalk.com/gettoken?appkey=${key}&appsecret=${secret}`,{method:'GET'})
+  const data = await res.json()
+  return data.access_token
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,22 @@
 import Head from 'next/head'
 import { ready, runtime } from 'dingtalk-jsapi'
 import { useRouter } from 'next/router'
-import { GetServerSideProps } from 'next'
 import { useEffectOnce } from '@/hooks/useEffectOnce'
+import useSwr from 'swr'
+import { useState } from 'react'
+
+function UserInfo(props: { code: string }) {
+  const  fetcher= () => fetch('/api/login', { method: 'POST', body: JSON.stringify({ code: props.code }) }).then(res => res.json())
+  const { data, error } = useSwr('/api/login',fetcher)
+  if (error) {
+    return <div>{error.message}</div>
+  } else {
+    return <div>{data?.data?.name}</div>
+  }
+}
+
 export default function Home() {
+  const [code, setCode] = useState<string>('')
   const router = useRouter()
   function getCorpIdFromUrl() {
     const asPath = router.asPath
@@ -14,13 +27,13 @@ export default function Home() {
       }
     }
   }
-  useEffectOnce(() => {
+  useEffectOnce(async () => {
     const corpId = getCorpIdFromUrl()
     if (corpId) {
       ready(async function () {
         try {
           const { code } = await runtime.permission.requestAuthCode({ corpId })
-          window.sessionStorage.setItem('code', code)
+          setCode(code)
         } catch (error) {
           console.error(error)
         }
@@ -34,16 +47,9 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className="py-20 px-12 text-center flex flex-col items-center gap-20px text-red-400">
-        <h1>UnoCss</h1>
+      <main>
+        {code && <UserInfo code={code} />}
       </main>
     </>
   )
-}
-
-export const getServerSideProps: GetServerSideProps = async () => {
-  return {
-    props: {
-    }
-  }
 }
