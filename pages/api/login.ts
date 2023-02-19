c66dfaca996607afab38faaaae020faf3ca91527
vino <vino0908@outlook.com>
2023-02-19 21:26:04 +0800

    feat: 添加 User Model

--- pages/api/login.ts
+++ pages/api/login.ts
@@ -1,31 +1,43 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 import { Result, UserInfo } from '../types'
+import prisma from '@/lib/prisma'
 
 export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Result<UserInfo|null>>
+  res: NextApiResponse<Result<UserInfo | null>>
 ) {
   req.headers['Content-Type'] = 'application/json'
-  if(req.method ==='POST'){
+  if (req.method === 'POST') {
     const token = await getAccessToken()
-    const result = await  fetch(`https://oapi.dingtalk.com/topapi/v2/user/getuserinfo?access_token=${token}`,{
-      method:'POST',
-      body:req.body,
-     })
+    const result = await fetch(`https://oapi.dingtalk.com/topapi/v2/user/getuserinfo?access_token=${token}`, {
+      method: 'POST',
+      body: req.body,
+    })
     const userData = await result.json()
-    if(userData.errcode === 0){
-      res.status(200).json({errcode:0,message:'获取用户信息成功',data:{userId:userData.result.userid,name:userData.result.name}})
-    }else{
-      res.status(500).json({errcode:1,message:'获取用户信息失败',data:null})
+    if (userData.errcode === 0) {
+      const user = await prisma.user.findFirst({ where: { userId: userData.result.userid } })
+      if (!user) {
+        await prisma.user.create({
+          data: {
+            userId: userData.result.userid,
+            name: userData.result.name,
+            createdAt: new Date(),
+            updatedAt: new Date(),
+          }
+        })
+      }
+      res.status(200).json({ errcode: 0, message: '获取用户信息成功', data: { userId: userData.result.userid, name: userData.result.name } })
+    } else {
+      res.status(500).json({ errcode: 1, message: '获取用户信息失败', data: null })
     }
   }
 }
 
 
-async function getAccessToken(){
-  const key= process.env.APP_KEY
+async function getAccessToken() {
+  const key = process.env.APP_KEY
   const secret = process.env.APP_SECRET
-  const res = await fetch(`https://oapi.dingtalk.com/gettoken?appkey=${key}&appsecret=${secret}`,{method:'GET'})
+  const res = await fetch(`https://oapi.dingtalk.com/gettoken?appkey=${key}&appsecret=${secret}`, { method: 'GET' })
   const data = await res.json()
   return data.access_token
 }
