84862f14de35ec5e2768f4ef051483a2560a96e0
vino <vino0908@outlook.com>
2023-02-21 00:04:30 +0800

    style: Nav 样式

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,50 +1,43 @@
 import Image from 'next/image'
 import { animated, useSpring } from '@react-spring/web'
-import { useRef } from 'react'
-
-const UserIcon = () => {
-  const divRef = useRef<HTMLDivElement>(null)
-  //获取div的宽度
-  const divWidth = divRef.current?.offsetWidth ?? 0
-  const [props, api] = useSpring(() => {
-    return {
-      x: -divWidth,
-    }
-  }, [])
-  return (
-    <>
-      <span
-        hover='b-black'
-        className='p-1 px-2 cursor-pointer border b-gray border-rd'>
-        王 鑫
-      </span>
-      <animated.div className="h-screen  fixed right-0 top-0 bg-blue" ref={divRef}>
-        <div>个人中心</div>
-      </animated.div>
-    </>
-  )
-}
+import { useState } from 'react'
 
 const Nav = () => {
-  const toggle = useRef(false)
-  const [props, api] = useSpring(
-    () => ({
-      x: -100,
-    }), [])
-  const userClick = () => {
-    api.start({ x: toggle.current ? 0 : 100 })
-    toggle.current = !toggle.current
-  }
+  const [hover, setHover] = useState(false)
+  const spring = useSpring({
+    from: {
+      width: 0,
+      height: 0,
+      opacity: 0,
+    },
+    to: {
+      width: hover ? 100 : 0,
+      height: hover ? 100 : 0,
+      opacity: 1,
+    },
+  })
   return (
-    <nav className="flex items-center justify-between flex-wrap bg-#fafafa p-2 px-4 ">
-      <Image
-        src="/logo.svg"
-        alt="logo"
-        width={36}
-        height={36}
-      />
-      <UserIcon />
-    </nav>
+    <nav className="flex items-center border-b">
+      <main className='m-auto bg-transparent w-1200 flex justify-between items-center p-8'>
+        <Image
+          src="/logo.svg"
+          alt="logo"
+          width={36}
+          height={36}
+        />
+        <div className='cursor-pointer position-relative'>
+          <span
+            onClick={() => setHover(!hover)}
+            hover="border-black bg-#f5f5f5"
+            className='border border-slate rounded-4 px-4'>王 鑫</span>
+          <animated.div className='border bg-#f5f5f5  overflow-hidden  position-absolute right-0 flex justify-center p-4' style={spring}>
+            <span>
+              个人中心
+            </span>
+          </animated.div>
+        </div>
+      </main>
+    </nav >
   )
 }
 
