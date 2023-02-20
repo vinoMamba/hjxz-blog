7e14e16d3062962d032956bec55c71156c624d48
vino <vino0908@outlook.com>
2023-02-20 18:36:16 +0800

    feat: 添加 react-spring

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,15 +1,49 @@
 import Image from 'next/image'
+import { animated, useSpring } from '@react-spring/web'
+import { useRef } from 'react'
+
+const UserIcon = () => {
+  const divRef = useRef<HTMLDivElement>(null)
+  //获取div的宽度
+  const divWidth = divRef.current?.offsetWidth ?? 0
+  const [props, api] = useSpring(() => {
+    return {
+      x: -divWidth,
+    }
+  }, [])
+  return (
+    <>
+      <span
+        hover='b-black'
+        className='p-1 px-2 cursor-pointer border b-gray border-rd'>
+        王 鑫
+      </span>
+      <animated.div className="h-screen  fixed right-0 top-0 bg-blue" ref={divRef}>
+        <div>个人中心</div>
+      </animated.div>
+    </>
+  )
+}
 
 const Nav = () => {
+  const toggle = useRef(false)
+  const [props, api] = useSpring(
+    () => ({
+      x: -100,
+    }), [])
+  const userClick = () => {
+    api.start({ x: toggle.current ? 0 : 100 })
+    toggle.current = !toggle.current
+  }
   return (
-    <nav className="flex items-center justify-between flex-wrap bg-#fafafa px-4">
+    <nav className="flex items-center justify-between flex-wrap bg-#fafafa p-2 px-4 ">
       <Image
         src="/logo.svg"
         alt="logo"
-        width={100}
-        height={100}
+        width={36}
+        height={36}
       />
-      <span className='p-1 px-2 cursor-pointer border b-slate border-rd'>王 鑫</span>
+      <UserIcon />
     </nav>
   )
 }
--- global.d.ts
+++ global.d.ts
@@ -1,5 +1,15 @@
 import { PrismaClient } from "@prisma/client"
+import * as React from 'react'
+
+declare module 'react' {
+  interface HTMLAttributes<T> extends AriaAttributes, DOMAttributes<T> {
+    hover?: string
+  }
+}
+
 declare global {
   var prisma: PrismaClient
 }
 export { }
+
+
