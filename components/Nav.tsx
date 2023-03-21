5911cbbc8475cb65ae80153ece63f46086c0bad8
vino <vino0908@outlook.com>
2023-03-21 09:38:51 +0800

    style: 修改nav 样式

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,7 +1,27 @@
+import Image from "next/image"
+import { useRouter } from "next/router"
+
 const Nav = () => {
+  const router = useRouter()
   return (
-    <nav>
-      nav
+    <nav
+      className="flex justify-between items-center h[64px] w-full position-fixed left-0 top-0 z-11 px-24 text-16 font-400 nav-bgf bg-transparent nav-letter nav-bs">
+      <div className='flex items-center cursor-pointer'>
+        <Image
+          onClick={() => router.push('/')}
+          src="/logo.svg"
+          alt="logo"
+          width={36}
+          height={36}
+        />
+        <h1 className='flex flex-col justify-center ml-4 '>
+          <span className='text-12 text-gray-5 font-italic'>HJ-Blog</span>
+          <span className='text-black op-80 mt-4'>技术博客</span>
+        </h1>
+      </div>
+      <div>
+
+      </div>
     </nav >
   )
 }
--- unocss.config.ts
+++ unocss.config.ts
@@ -8,7 +8,10 @@ export default defineConfig({
     presetWebFonts(),
   ],
   rules: [
-    [/^text-(\d+)$/, ([_, match]) => ({ fontSize: `${match}px` })]
+    [/^text-(\d+)$/, ([_, match]) => ({ fontSize: `${match}px` })],
+    ['nav-bgf', { 'backdrop-filter': 'blur(20px)' }],
+    ['nav-letter', { 'letter-spacing': '1px' }],
+    ['nav-bs', { 'box-shadow': '0 2px 4px rgb(0 0 0 / 12%)' }],
   ],
   shortcuts: {}
 })
